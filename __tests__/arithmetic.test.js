512e9e19bf8dbf2109e60efe76f3cf2b5c8f89a0
adrianagraybill <adriana.graybill@gmail.com>
2019-06-25 21:36:37 -0700

    math done

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,3 +2,26 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+describe('arithmetic addition', () => {
+  it('can add 2 numbers', () => {
+    expect(arithmetic.add()).toBe();
+  });
+});
+
+describe('arithmetic addition', () => {
+  it('hates words', () => {
+    expect(arithmetic.add('friend')).toBe(null);
+  });
+});
+
+describe('arithmetic addition', () => {
+  it('can add 2 numbers', () => {
+    expect(arithmetic.subtract(1,2)).toBe(1+2);
+  });
+});
+
+describe('arithmetic addition', () => {
+  it('hates words', () => {
+    expect(arithmetic.subtract('hello','friend')).toBe(null);
+  });
+});
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const greet = require('./lib/greet.js');
-const math = require('./lib/arithmetic.js');
-console.log(greet('JOHN'));
-console.log(math.add(1,3)); // 4
-console.log(math.subtract(1,3)); // -2
+const arithmetic = require('./lib/arithmetic.js');
+
+console.log(arithmetic.add(1,3,6)); // 4
+console.log(arithmetic.subtract(1,3,6)); // -2
+console.log(arithmetic.multiply(5,3,6)); // -2
+console.log(arithmetic.divide(1,4)); // -2
